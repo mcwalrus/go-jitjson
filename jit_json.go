6ccb8c962116907ef2dba4ad02feb999208a1cd0
Max Collier <max.collier@xerra.nz>
2022-10-08 01:29:53 +1300

    added JitJSON method Set and removed method Validate

--- jit_json.go
+++ jit_json.go
@@ -5,18 +5,20 @@ import (
 	"fmt"
 )
 
-// TODO need to handle
-type JitJSON[T JSON] struct {
-	data []byte
-	val  *T
-}
-
-// TODO rename.
+// JSON struct requires marshal and unmarshal json methods or json tags.
 type JSON interface {
 	json.Marshaler
 	json.Unmarshaler
 }
 
+// JitJSON provides 'just-in-time' complication for both marshalling and
+// unmarshalling some JSON struct.
+type JitJSON[T JSON] struct {
+	data []byte
+	val  *T
+}
+
+// NewJitJSON creates new JitJSON from data.
 func NewJitJSON[T JSON](data []byte) (*JitJSON[T], error) {
 	if !json.Valid(data) {
 		return nil, fmt.Errorf("invalid json")
@@ -29,21 +31,22 @@ func NewJitJSON[T JSON](data []byte) (*JitJSON[T], error) {
 	return &jit, nil
 }
 
-// TODO: pretty sure
-func (jit *JitJSON[JSON]) Validate() error {
-	if jit.data == nil && jit.val == nil {
-		return fmt.Errorf("jit-json missing value")
-	}
-	return nil
+// Set new value to jitJSON.
+// JitJSON byte representation is wiped to avoid data mismatch.
+func (jit *JitJSON[JSON]) Set(val JSON) {
+	jit.data = nil
+	jit.val = &val
 }
 
+// Marshal provides the wrapper function to json.Marshal.
+// JitJSON caches the byte representation once marshal has occurred.
 func (jit *JitJSON[JSON]) Marshal() ([]byte, error) {
 	if jit.data != nil {
 		return jit.data, nil
 	}
 
 	var err error
-	jit.data, err = json.Marshal(&jit.val)
+	jit.data, err = json.Marshal(jit.val)
 	if err != nil {
 		return nil, err
 	}
@@ -51,16 +54,18 @@ func (jit *JitJSON[JSON]) Marshal() ([]byte, error) {
 	return jit.data, nil
 }
 
-func (jit *JitJSON[JSON]) Unmarshal() (*JSON, error) {
+// Unmarshal provides the wrapper function to json.Unmarshal.
+// JitJSON caches the returned value once unmarshal has occurred.
+func (jit *JitJSON[JSON]) Unmarshal() (JSON, error) {
 	if jit.val != nil {
-		return jit.val, nil
+		return *jit.val, nil
 	}
 
-	var err error
-	err = json.Unmarshal(jit.data, &jit.val)
+	var val JSON
+	err := json.Unmarshal(jit.data, jit.val)
 	if err != nil {
-		return nil, err
+		return val, err
 	}
 
-	return jit.val, nil
+	return *jit.val, nil
 }
