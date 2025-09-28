2a3b4f6a457047b8bef01348c5c36b516f51e6a3
Max Collier <max.collier@entaingroup.com.au>
2025-09-28 16:57:40 +1000

    re-approaching jitjson v1/v2/custom

--- jsonv2.go
+++ jsonv2.go
@@ -7,34 +7,102 @@ import (
 	jsonv2 "encoding/json/v2"
 )
 
-// jsonParserV2 is a JSONParser using the encoding/json/v2 package.
-type jsonParserV2 struct{}
+var _ jsonv2.MarshalerTo = (*JitJSON_V2[any])(nil)
+var _ jsonv2.UnmarshalerFrom = (*JitJSON_V2[any])(nil)
 
-var _ JSONParser = (*jsonParserV2)(nil)
-
-func (j *jsonParserV2) Name() string {
-	return "encoding/json/v2"
+// JitJSON_V2 provides just-in-time (JIT) JSON parsing in Go for a value of type T.
+// Parsing to or from JSON is deferred until the Marshal and Unmarshal methods are called.
+// You can think of JitJSON[T] as a lazy two way JSON parser with results caching implemented.
+type JitJSON_V2[T any] struct {
+	data []byte
+	val  *T
 }
 
-func (j *jsonParserV2) Marshal(v interface{}) ([]byte, error) {
-	return jsonv2.Marshal(v)
+// New creates JitJSON[T] from a value, with the default parser set.
+func New_V2[T any](val T) *JitJSON_V2[T] {
+	return &JitJSON[T]{val: &val}
 }
 
-func (j *jsonParserV2) Unmarshal(data []byte, v interface{}) error {
-	return jsonv2.Unmarshal(data, v)
+// NewFromBytes creates a JitJSON[T] from an encoding, with the default parser set.
+// If the encoding is invalid JSON, an error will be observed once Marshal is called.
+func NewFromBytes_V2[T any](data []byte) *JitJSON_V2[T] {
+	return &JitJSON[T]{data: data}
 }
 
-func init() {
-	initParserRegistry()
-	MustRegisterParser(&jsonParserV2{})
+// Set sets a new value to JitJSON[T].
+func (jit *JitJSON_V2[T]) Set(val T) {
+	jit.val = &val
+	jit.data = nil
 }
 
-var _ jsonv2.MarshalerTo = (*JitJSON[any])(nil)
-var _ jsonv2.UnmarshalerFrom = (*JitJSON[any])(nil)
+// SetBytes sets a new encoding to JitJSON[T].
+func (jit *JitJSON_V2[T]) SetBytes(data []byte) {
+	jit.val = nil
+	jit.data = data
+}
+
+// Marshal performs deferred json marshaling for the value of JitJSON[T]. The method can return without evaluating
+// 'json.Marshal' if the value has been marshaled previously. Once marshaled, the encoded value is stored with the
+// jitjson for future retrieval. If there is no value to marshal, the method returns nil, nil.
+func (jit *JitJSON_V2[T]) Marshal() ([]byte, error) {
+	if jit.data != nil {
+		return jit.data, nil
+	}
+	if jit.val == nil {
+		return nil, nil
+	}
+
+	var err error
+	jit.data, err = jsonv2.Marshal(jit.val)
+	if err != nil {
+		return nil, err
+	}
+
+	return jit.data, nil
+}
+
+// Unmarshal performs deferred json unmarshaling for the value of JitJSON[T]. The method can return without evaluating
+// 'json.Unmarshal' if the value has been unmarshaled previously. Once unmarshaled, the decoded value is stored with
+// the jitjson for future retrieval. If there is no JSON data to unmarshal, the zero value of type T is returned.
+// If the JSON data does not unmarshal into the type T, the method will return an error.
+func (jit *JitJSON_V2[T]) Unmarshal() (T, error) {
+	if jit.val != nil {
+		return *jit.val, nil
+	}
+	var val T
+	if jit.data == nil {
+		return val, nil
+	}
+
+	jit.val = &val
+	err := jsonv2.Unmarshal(jit.data, jit.val)
+	if err != nil {
+		return val, err
+	}
+
+	return *jit.val, nil
+}
+
+// MarshalJSON can be used to marshal JitJSON[T] to JSON.
+// This method is compatable with the encoding/json/v1.Marshaler interface.
+// When using encoding/json/v2, the json.MarshalJSONTo method is used to marshal instead.
+func (jit *JitJSON_V2[T]) MarshalJSON() ([]byte, error) {
+	return jit.Marshal()
+}
+
+// UnmarshalJSON stores JSON data to be unmarshaled later.
+// This method is compatable with the encoding/json/v1.Unmarshaler interface.
+// When using encoding/json/v2, the json.UnmarshalJSONTo method is used to unmarshal instead.
+func (jit *JitJSON_V2[T]) UnmarshalJSON(data []byte) error {
+	jit.val = nil
+	jit.data = data
+	return nil
+}
 
 // MarshalJSONTo implements the encoding/json/v2.MarshalerTo interface for efficient
 // streaming JSON marshaling with json/v2. This method writes the JSON representation
-// directly to the provided jsontext.Encoder.
+// directly to the provided jsontext.Encoder, which is considered more efficient than
+// the encoding/json/v1.MarshalJSON method.
 func (jit *JitJSON[T]) MarshalJSONTo(enc *jsontext.Encoder) error {
 	data, err := jit.Marshal()
 	if err != nil {
@@ -47,8 +115,9 @@ func (jit *JitJSON[T]) MarshalJSONTo(enc *jsontext.Encoder) error {
 }
 
 // UnmarshalJSONFrom implements the encoding/json/v2.UnmarshalerFrom interface for
-// efficient streaming JSON unmarshaling with json/v2. This method reads JSON data
-// directly from the provided jsontext.Decoder.
+// more efficient decoding with JSON with json/v2 library. This method reads JSON
+// data directly from the provided jsontext.Decoder, which is considered more efficient
+// than the encoding/json/v1.UnmarshalJSON method.
 func (jit *JitJSON[T]) UnmarshalJSONFrom(dec *jsontext.Decoder) error {
 	value, err := dec.ReadValue()
 	if err != nil {
