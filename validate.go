791f903ce1637c5e5bb8e380753ab84d225e534c
Max Collier <collierwm01@gmail.com>
2025-10-01 22:20:29 +1000

    vaildation test coverage

--- validate.go
+++ validate.go
@@ -2,29 +2,26 @@ package jitjson
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 )
 
 // Validate checks if a type T can be marshaled or unmarshaled to/from JSON.
 // This is useful to validate types at runtime before using them with JitJSON.
-// Note, jitjson types are simply wrappers around [json.Marshal] and [json.Unmarshal].
+// Note, jitjson are simply wrappers around [json.Marshal] and [json.Unmarshal].
 //
 // Example:
 //
-//	if err := jitjson.Validate[map[string]string](); err != nil {
-//		fmt.Println("map[string]string is JSON marshalable")
-//	}
-//
-//	if err := jitjson.Validate[map[int]string](); err == nil {
+//	// Key is not comparable
+//	if err := jitjson.Validate[map[int]string](); err != nil {
 //		fmt.Println("map[int]string is not JSON marshalable")
 //	}
 func Validate[T any]() error {
 	var zero T
-	if _, err := json.Marshal(&zero); err != nil {
-		return fmt.Errorf("type %T is not JSON marshalable: %w", zero, err)
-	}
-	if err := json.Unmarshal([]byte("null"), &zero); err != nil {
-		return fmt.Errorf("type %T is not JSON unmarshalable: %w", zero, err)
+	_, err1 := json.Marshal(&zero)
+	err2 := json.Unmarshal([]byte("null"), &zero)
+	if err1 != nil || err2 != nil {
+		return fmt.Errorf("type %T is not parseable: %w", zero, errors.Join(err1, err2))
 	}
 	return nil
 }
